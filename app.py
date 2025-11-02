03de91ca460ae30ab96b65634f17ade109d0c17c
AdelodunAbraham <ab.adelodun@gmail.com>
2025-11-02 16:21:50 +0100

    Add main entry point to run the Flask application

--- app.py
+++ app.py
@@ -70,3 +70,8 @@ def analyze_image():
 
     return jsonify({"results": response}), 200
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
